4739743d65bc7ac40b735a094c986ac0d59793f7
andrewhall1124 <andrewmartinhall2@gmail.com>
2025-03-12 13:52:15 -0600

    Added ids file.

--- pipelines/barra_file.py
+++ pipelines/barra_file.py
@@ -33,6 +33,7 @@ class ZipFolder(Enum):
     SMD_USSLOW_100_D = "SMD_USSLOW_100_D"  # _2025
     SMD_USSLOWL_100_D = "SMD_USSLOWL_100_D"
     SMD_USSLOWL_100 = "SMD_USSLOWL_100"
+    SMD_USSLOW_XSEDOL_ID = "SMD_USSLOW_XSEDOL_ID"
 
 
 class File(Enum):
@@ -42,6 +43,8 @@ class File(Enum):
     USSLOW_100_Asset_DlySpecRet = "USSLOW_100_Asset_DlySpecRet"
     USSLOWL_100_Covariance = "USSLOWL_100_Covariance"
     USSLOWL_100_DlyFacRet = "USSLOWL_100_DlyFacRet"
+    USA_XSEDOL_Asset_ID = "USA_XSEDOL_Asset_ID"
+
 
 @dataclass
 class BarraFile:
@@ -73,10 +76,9 @@ class BarraFile:
     def zip_folder_name(self) -> str:
         match self.folder:
             case Folder.HISTORY:
-                return f"{self.zip_folder.value}_{self.date_.year}"            
+                return f"{self.zip_folder.value}_{self.date_.year}"
             case Folder.BIME:
                 return f"{self.zip_folder.value}_{self.date_.strftime('%y%m%d')}"
-
 
     @property
     def zip_folder_path(self):
@@ -85,7 +87,7 @@ class BarraFile:
     @property
     def file_name(self):
         return f"{self.file.value}.{self.date_.strftime('%Y%m%d')}"
-    
+
     @property
     def file_path(self) -> str:
         return self.base_path / self.zip_folder_name / self.file_name
@@ -96,10 +98,10 @@ class BarraFile:
         try:
             with ZipFile(self.zip_folder_path, "r") as zip_ref:
                 return self.file_name in zip_ref.namelist()
-            
+
         except FileNotFoundError:
             return False
-    
+
     @property
     def df(self) -> pl.DataFrame:
         """Task for getting a file given a BarraFile."""
@@ -120,6 +122,8 @@ class BarraFile:
                         skip = 2
                     case File.USSLOWL_100_DlyFacRet:
                         skip = 2
+                    case File.USA_XSEDOL_Asset_ID:
+                        skip = 1
                     case _:
                         msg = "Not a valid barra_file.file."
                         raise ValueError(msg)
@@ -130,11 +134,38 @@ class BarraFile:
 if __name__ == "__main__":
     barra_file = BarraFile(
         folder=Folder.BIME,
-        zip_folder=ZipFolder.SMD_USSLOWL_100,
-        file=File.USSLOWL_100_DlyFacRet,
-        date_=date(2025, 3, 6),
+        zip_folder=ZipFolder.SMD_USSLOW_XSEDOL_ID,
+        file=File.USA_XSEDOL_Asset_ID,
+        date_=date(2025, 3, 7),
     )
 
-    print(barra_file.zip_folder_path)
-    print(barra_file.file_path)
-    print(barra_file.df)
+    # print(barra_file.zip_folder_path)
+    # print(barra_file.file_path)
+    # print(barra_file.df)
+
+    df = (
+        barra_file.df
+        .rename({
+            '!Barrid': 'barrid',
+            'AssetIDType': 'asset_id_type',
+            'AssetID': 'asset_id',
+            'StartDate': 'start_date',
+            'EndDate': 'end_date'
+        })
+    )
+    df = (
+        df.with_columns(
+            pl.col(['start_date', 'end_date']).cast(pl.String).str.strptime(dtype=pl.Date, format="%Y%m%d")
+        )
+        .filter(pl.col('barrid').ne('[End of File]'))
+        .with_columns(pl.col('end_date').clip(upper_bound=date.today()))
+        .pivot(index=['start_date', 'end_date', 'barrid'], on='asset_id_type', values='asset_id')
+        .with_columns(
+            pl.date_ranges("start_date", "end_date").alias("date")
+        )
+        .explode('date')
+        .rename({col: col.lower() for col in df.columns})
+        .select(['date', 'barrid', 'cins', 'cusip', 'isin', 'localid'])
+    )
+
+    print(df)
