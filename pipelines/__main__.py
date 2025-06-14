0c0bdcfae895946d720273f547ef4800cb6919c1
andrewhall1124 <andrewmartinhall2@gmail.com>
2025-06-14 14:40:47 -0600

    Soft reset.

--- pipelines/__main__.py
+++ pipelines/__main__.py
@@ -15,62 +15,61 @@ from crsp_monthly_flow import crsp_monthly_backfill_flow
 from crsp_events_flow import crsp_events_backfill_flow
 from barra_factors_flow import barra_factors_daily_flow
 from covariance_matrix_flow import covariance_daily_flow
-from datetime import date
+import datetime as dt
 
-def barra_daily_flow() -> None:
-    # Assets table
-    barra_returns_daily_flow()
-    barra_specific_returns_daily_flow()
-    barra_risk_daily_flow()
-    barra_volume_daily_flow()
+# def barra_daily_flow() -> None:
+#     # Assets table
+#     barra_returns_daily_flow()
+#     barra_specific_returns_daily_flow()
+#     barra_risk_daily_flow()
+#     barra_volume_daily_flow()
 
-    # Covariance Matrix Components
-    barra_exposures_daily_flow()
-    barra_covariances_daily_flow()
+#     # Covariance Matrix Components
+#     barra_exposures_daily_flow()
+#     barra_covariances_daily_flow()
 
-    # Factors
-    barra_factors_daily_flow()
+#     # Factors
+#     barra_factors_daily_flow()
 
-def barra_history_flow(start_date: date, end_date: date) -> None:
+def barra_history_flow(start_date: dt.date, end_date: dt.date) -> None:
     # Assets table
     barra_returns_history_flow(start_date, end_date)
-    barra_specific_returns_history_flow(start_date, end_date)
-    barra_risk_history_flow(start_date, end_date)
-    barra_volume_history_flow(start_date, end_date)
+    # barra_specific_returns_history_flow(start_date, end_date)
+    # barra_risk_history_flow(start_date, end_date)
+    # barra_volume_history_flow(start_date, end_date)
 
-    # Covariance Matrix Components
-    barra_exposures_history_flow(start_date, end_date)
-    barra_covariances_history_flow(start_date, end_date)
+    # # Covariance Matrix Components
+    # barra_exposures_history_flow(start_date, end_date)
+    # barra_covariances_history_flow(start_date, end_date)
 
-def id_mappings_flow() -> None:
-    barra_ids_daily_flow()
-    barra_assets_daily_flow()
+# def id_mappings_flow() -> None:
+#     barra_ids_daily_flow()
+#     barra_assets_daily_flow()
 
-def ftse_history_flow(start_date: date, end_date: date) -> None:
-    ftse_russell_backfill_flow(
-        start_date=start_date,
-        end_date=end_date,
-    )
+# def ftse_history_flow(start_date: dt.date, end_date: dt.date) -> None:
+#     """Requires log in."""
+#     ftse_russell_backfill_flow(
+#         start_date=start_date,
+#         end_date=end_date,
+#     )
 
-def crsp_history_flow(start_date: date, end_date: date) -> None:
-    crsp_events_backfill_flow(start_date, end_date)
-    crsp_monthly_backfill_flow(start_date, end_date)
-    crsp_daily_backfill_flow(start_date, end_date)
+# def crsp_history_flow(start_date: dt.date, end_date: dt.date) -> None:
+#     crsp_events_backfill_flow(start_date, end_date)
+#     crsp_monthly_backfill_flow(start_date, end_date)
+#     crsp_daily_backfill_flow(start_date, end_date)
 
-def strategy_backfill_flow(start_date: date, end_date: date) -> None:
-    signals_history_flow(start_date, end_date)
-    active_weights_history_flow(start_date, end_date)
-    risk_parity_history_flow(start_date, end_date)
+# def strategy_backfill_flow(start_date: dt.date, end_date: dt.date) -> None:
+#     signals_history_flow(start_date, end_date)
+#     active_weights_history_flow(start_date, end_date)
+#     risk_parity_history_flow(start_date, end_date)
 
-def daily_etl_pipeline() -> None:
-    barra_daily_flow()
-    id_mappings_flow()
-    covariance_daily_flow()
+# def daily_pipeline() -> None:
+#     barra_daily_flow()
+#     id_mappings_flow()
+#     covariance_daily_flow()
 
-    # TODO: Work in progress for strategy data
-    # date_ = date.today() - timedelta(days=1)
-    # strategy_backfill_flow(date_, date_)
-    
-if __name__ == '__main__':
-    daily_etl_pipeline()
+
+def history_etl_pipeline(start_date: dt.date, end_date: dt.date, database: str) -> None:
+    barra_returns_history_flow(start_date, end_date, database)
+    pass
 
