626fee618782e2b8baf3e5a870c94396de461b51
andrewhall1124 <andrewmartinhall2@gmail.com>
2025-07-26 12:33:33 -0600

    Comments.

--- pipelines/composite_alphas.py
+++ pipelines/composite_alphas.py
@@ -22,28 +22,34 @@ def compute_composite_alphas(start_date: date, end_date: date, database: Databas
 
     signal_portfolios = (
         assets.join(weights, on=["date", "barrid"], how="left")
+        # Constrain the leverage of each signal portfolio to be one.
         .with_columns(
             pl.col("weight")
             .truediv(pl.col("weight").abs().sum())
             .over(["barrid", "signal"])
         )
+        # Lag weights
         .with_columns(pl.col("weight").shift(1).over(["barrid", "signal"]))
         .group_by(["date", "signal"])
+        # Compute the return of each signal portfolio
         .agg(pl.col("return").mul(pl.col("weight")).sum())
     )
     print(signal_portfolios.collect())
 
     risk_parity_weights = (
         signal_portfolios.sort(["signal", "date"])
+        # Find the 22 day volatility of each signal portfolio
         .with_columns(
             pl.col("return")
             .rolling_std(window_size=22)
             .over("signal")
             .alias("volatility")
         )
+        # Use the inverse of the volatility as the weight
         .with_columns(
             pl.lit(1).truediv(pl.col("volatility")).over("date").alias("weight")
         )
+        # Constraint weights to sum to one
         .with_columns(pl.col("weight").truediv(pl.col("weight").sum()).over("date"))
     )
     print(risk_parity_weights.collect())
@@ -52,6 +58,7 @@ def compute_composite_alphas(start_date: date, end_date: date, database: Databas
         signals.join(risk_parity_weights, on=["date", "signal"], how="left")
         .drop_nulls("weight")
         .group_by(["date", "barrid"])
+        # Compute risk parity weighted alphas
         .agg(pl.col("weight").mul(pl.col("alpha")).sum().alias("alpha"))
         .sort(["barrid", "date"])
         .select("date", "barrid", pl.lit("risk_parity").alias("name"), "alpha")
