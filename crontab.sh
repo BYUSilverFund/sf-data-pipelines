2198f2e04a1742a5d6ced54a4c7902fe58c5295c
andrewhall1124 <andrewmartinhall2@gmail.com>
2025-10-19 09:40:14 -0600

    Updated production flow.

--- crontab.sh
+++ crontab.sh
@@ -6,6 +6,7 @@ TEMP_CRON=$(mktemp)
 
 cat > "$TEMP_CRON" << 'EOF'
 0 2 * * * cd /home/amh1124/Projects/sf-data-pipelines && .venv/bin/python -m sf_data_pipelines covariance-matrix >> logs/covariance_matrix.log 2>&1
+0 2 * * * cd /home/amh1124/Projects/sf-data-pipelines && .venv/bin/python -m sf_data_pipelines barra update --database production >> logs/production_database.log 2>&1
 EOF
 
 crontab "$TEMP_CRON"
